262be9ac229853daa00e9ba7f318f26382fc444d
ndg <dooh22@duksung.ac.kr>
2024-10-10 09:30:34 +0900

    n<0 부분 삭제함

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 // computes a factorial of a given positive integer
 // version 1 -> 2 -> 3
 function factorial(n) { // 5! = 5 * 4 * 3 * 2 * 1 = 120
-
+/*
     if( n < 0 ) {
         throw new Error('n must be a positive integer');
     }
-
+*/
 
     if( n === 0 ) {
         return 1;
     }
 
-    return n * factorial(n - 1); // 재귀적 호출
+    return n * factorial(n - 1); // 재귀적 호출 중요!
 }
 
 module.exports = factorial;
